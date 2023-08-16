e2f3709a9170fc9a75586e3758008c20a446f5d3
dylankaifesh <kaifeshd@gmail.com>
2023-08-16 08:55:24 -0500

    initial

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-#main
+import pygame
+import time
+import random
+
+WIDTH, HEIGHT = 500,500
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Space Dodge")
+
+BG = pygame.image.load("bg.jpg")
+
+def main():
+    run = True
+
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+               run = False
+               break
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
