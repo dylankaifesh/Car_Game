02b4bbec5e613c33f87a3499baa535624f731390
dylankaifesh <kaifeshd@gmail.com>
2023-08-17 15:36:50 -0500

    start

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import time
 import random
+pygame.font.init()
 
 WIDTH, HEIGHT = 1000,800
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -12,8 +13,13 @@ PLAYER_WIDTH = 40
 PLAYER_HEIGHT = 60
 PLAYER_VEL = 5
 
-def draw(player):
+FONT = pygame.font.SysFont("comicsans", 30)
+
+def draw(player, elapsed_time):
     WIN.blit(BG,(0,0))
+
+    time_text = FONT.render(f"Time:{round(elapsed_time)}s", 1, "white")
+    WIN.blit(time_text, (10,10))
 
     pygame.draw.rect(WIN, "red", player) # you can do the RGB color code or write in the color
 
@@ -25,24 +31,32 @@ def main():
     player = pygame.Rect(200, HEIGHT - PLAYER_HEIGHT, PLAYER_WIDTH, PLAYER_HEIGHT)
 
     clock = pygame.time.Clock()
+    start_time = time.time()
+    elapsed_time = 0
 
     while run:
         clock.tick(60)
+        elapsed_time = time.time() - start_time
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                run = False
                break
 
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT]:
+        if keys[pygame.K_LEFT] and player.x - PLAYER_VEL >= 0:
             player.x -= PLAYER_VEL
-        if keys[pygame.K_RIGHT]:
+        if keys[pygame.K_RIGHT]and player.x + PLAYER_VEL + player.width <= WIDTH:
             player.x += PLAYER_VEL
+        if keys[pygame.K_UP] and player.y - PLAYER_VEL >= 0:
+            player.y -= PLAYER_VEL
+        if keys[pygame.K_DOWN]and player.y + PLAYER_VEL + player.height <= HEIGHT:
+            player.y += PLAYER_VEL
 
-
-        draw(player)
+        draw(player, elapsed_time)
 
     pygame.quit()
 
 if __name__ == "__main__":
     main()
+
+    #27:38
