273a882193dad47e5435a85ffbc3884f345c43ec
dylankaifesh <kaifeshd@gmail.com>
2023-08-17 14:11:12 -0500

    setup

--- main.py
+++ main.py
@@ -2,20 +2,45 @@ import pygame
 import time
 import random
 
-WIDTH, HEIGHT = 500,500
+WIDTH, HEIGHT = 1000,800
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Space Dodge")
 
-BG = pygame.image.load("bg.jpg")
+BG = pygame.transform.scale(pygame.image.load("bg.jpg"), (WIDTH, HEIGHT))
+
+PLAYER_WIDTH = 40
+PLAYER_HEIGHT = 60
+PLAYER_VEL = 5
+
+def draw(player):
+    WIN.blit(BG,(0,0))
+
+    pygame.draw.rect(WIN, "red", player) # you can do the RGB color code or write in the color
+
+    pygame.display.update()
 
 def main():
     run = True
+    
+    player = pygame.Rect(200, HEIGHT - PLAYER_HEIGHT, PLAYER_WIDTH, PLAYER_HEIGHT)
+
+    clock = pygame.time.Clock()
 
     while run:
+        clock.tick(60)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                run = False
                break
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            player.x -= PLAYER_VEL
+        if keys[pygame.K_RIGHT]:
+            player.x += PLAYER_VEL
+
+
+        draw(player)
 
     pygame.quit()
 
